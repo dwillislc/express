068af2558e19ff5bbbffd3625e237befb0592a0f
David Willis <dwillislc@gmail.com>
2018-12-15 16:32:58 -0500

    updated about and index pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express'); 
 const router = express.Router(); 
+const { data } = require('../data/data.json'); 
+const { projects } = data; 
+
 
 // Index route to handle requests to "Home" 
-router.get('/', (req, res) => {
-	res.render('index'); 
+router.get('/', (req, res) => {	
+	res.render('index', {projects}); 
 });
 
 // Route to handle requests to "About"
